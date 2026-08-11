6dfcabf3f034d6e6f7af1d28aeb9e0ea18b91422
garmash515-gif <garmash515@gmail.com>
2026-08-11 23:30:49 +0300

    fix: bust module cache for new molecule layout

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import { createAppScene } from './scene/scene.js';
-import { createDopamineMolecule } from './molecule/dopamine.js';
-import { createFireflies } from './effects/fireflies.js';
+import { createAppScene } from './scene/scene.js?v=13.8.1';
+import { createDopamineMolecule } from './molecule/dopamine.js?v=13.8.1';
+import { createFireflies } from './effects/fireflies.js?v=13.8.1';
 
 const app = createAppScene(document.querySelector('#stage'));
 
