b6ec3e7bef7cdc639d04da1dd9e93f1a7510c403
garmash515-gif <garmash515@gmail.com>
2026-08-12 01:22:40 +0300

    15.3.1 replace benzene ring with dopamine molecular graph

--- src/molecule/dopamine.js
+++ src/molecule/dopamine.js
@@ -1,50 +1,62 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.185.1/build/three.module.js';
+import { atom } from './atoms.js';
+import { bond } from './bonds.js';
 import { createAtom } from './atom.js';
-import { createBond } from './bond.js';
+import { createMoleculeView } from './moleculeView.js';
 import { createEnergyPulse } from '../effects/energyFlow.js';
 
-export function createDopamineMolecule(scene) {
-  const molecule = new THREE.Group();
-  const atoms = [];
-  const bonds = [];
-
-  const radius = 0.72;
-  const atomSize = 0.105;
-
+export function createDopamineGraph() {
+  const r = 0.72;
+  const ring = [];
   for (let i = 0; i < 6; i++) {
-    const angle = Math.PI / 2 + i * Math.PI / 3;
-
-    const atom = createAtom(
-      'C',
-      {
-        x: Math.cos(angle) * radius,
-        y: Math.sin(angle) * radius,
-        z: 0
-      },
-      atomSize
-    );
-
-    molecule.add(atom);
-    atoms.push(atom);
+    const a = Math.PI / 2 + i * Math.PI / 3;
+    ring.push(atom(`C${i + 1}`, 'C', { x: Math.cos(a) * r, y: Math.sin(a) * r, z: 0 }));
   }
 
-  for (let i = 0; i < atoms.length; i++) {
-    const start = atoms[i];
-    const end = atoms[(i + 1) % atoms.length];
-    const bond = createBond(start, end);
+  const atoms = [...ring,
+    atom('O1', 'O', { x: -0.36, y: 1.14, z: 0 }), atom('H_O1', 'H', { x: -0.36, y: 1.43, z: 0 }),
+    atom('O2', 'O', { x: -0.98, y: 0.42, z: 0 }), atom('H_O2', 'H', { x: -1.24, y: 0.56, z: 0 }),
+    atom('C7', 'C', { x: 0.98, y: -1.14, z: 0 }), atom('C8', 'C', { x: 1.38, y: -1.82, z: 0 }),
+    atom('N1', 'N', { x: 1.78, y: -2.50, z: 0 }), atom('H_N1', 'H', { x: 2.05, y: -2.76, z: 0 }),
+    atom('H_C3', 'H', { x: -0.98, y: -0.42, z: 0 }), atom('H_C5', 'H', { x: 0.98, y: 0.42, z: 0 }), atom('H_C6', 'H', { x: 1.24, y: 1.00, z: 0 })
+  ];
 
-    molecule.add(bond);
-    bonds.push({
-      start,
-      end,
-      object: bond,
-      trigger() {
-        return createEnergyPulse(start.position, end.position);
-      }
-    });
+  const bonds = [];
+  const ringTypes = ['double', 'single', 'double', 'single', 'double', 'single'];
+  for (let i = 0; i < 6; i++) bonds.push(bond(ring[i].id, ring[(i + 1) % 6].id, ringTypes[i]));
+  bonds.push(
+    bond('C1', 'O1'), bond('O1', 'H_O1'), bond('C2', 'O2'), bond('O2', 'H_O2'),
+    bond('C4', 'C7'), bond('C7', 'C8'), bond('C8', 'N1'), bond('N1', 'H_N1'),
+    bond('C3', 'H_C3'), bond('C5', 'H_C5'), bond('C6', 'H_C6')
+  );
+  return { atoms, bonds };
+}
+
+export function createDopamineMolecule(scene) {
+  const graph = createDopamineGraph();
+  const molecule = createMoleculeView(graph);
+  const atomMap = new Map();
+  molecule.userData.atomMap = atomMap;
+
+  for (const data of graph.atoms) {
+    const size = data.element === 'C' ? 0.105 : data.element === 'O' ? 0.13 : 0.065;
+    const view = createAtom(data.element, data.position, size);
+    view.userData.id = data.id;
+    atomMap.set(data.id, view);
+    molecule.add(view);
   }
 
+  const viewBonds = [];
+  for (const data of graph.bonds) {
+    const start = atomMap.get(data.from);
+    const end = atomMap.get(data.to);
+    const object = molecule.userData.createBondView(start, end, data.type);
+    molecule.add(object);
+    viewBonds.push({ start, end, object, type: data.type, trigger() {
+      return createEnergyPulse(start.position, end.position);
+    }});
+  }
+
+  molecule.userData.bonds = viewBonds;
   scene.add(molecule);
-
-  return { molecule, atoms, bonds };
+  return { molecule, atoms: [...atomMap.values()], bonds: viewBonds, graph };
 }
