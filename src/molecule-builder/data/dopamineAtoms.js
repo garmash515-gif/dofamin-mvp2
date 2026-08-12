e1d0b378b2812588038f0a9c84fa6de111693fa1
garmash515-gif <garmash515@gmail.com>
2026-08-12 21:06:37 +0300

    Add initial dopamine atom coordinate map

---
+++ src/molecule-builder/data/dopamineAtoms.js
@@ -0,0 +1,47 @@
+export const dopamineAtoms = [
+  {
+    id: 'C1',
+    element: 'carbon',
+    position: [-1.5, 0.8, 0]
+  },
+  {
+    id: 'C2',
+    element: 'carbon',
+    position: [-0.5, 1.5, 0]
+  },
+  {
+    id: 'C3',
+    element: 'carbon',
+    position: [0.7, 1.0, 0]
+  },
+  {
+    id: 'C4',
+    element: 'carbon',
+    position: [0.8, -0.3, 0]
+  },
+  {
+    id: 'C5',
+    element: 'carbon',
+    position: [-0.2, -1.0, 0]
+  },
+  {
+    id: 'C6',
+    element: 'carbon',
+    position: [-1.4, -0.5, 0]
+  },
+  {
+    id: 'O1',
+    element: 'oxygen',
+    position: [-2.3, 1.5, 0]
+  },
+  {
+    id: 'O2',
+    element: 'oxygen',
+    position: [1.6, 1.5, 0]
+  },
+  {
+    id: 'N1',
+    element: 'nitrogen',
+    position: [2.8, -0.2, 0]
+  }
+];
