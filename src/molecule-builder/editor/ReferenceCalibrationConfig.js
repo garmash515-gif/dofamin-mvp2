34d99f9f349fe6d2532ec0fef5a255d897ce45cd
garmash515-gif <garmash515@gmail.com>
2026-08-12 21:10:59 +0300

    Add reference calibration config for molecule grid setup

---
+++ src/molecule-builder/editor/ReferenceCalibrationConfig.js
@@ -0,0 +1,16 @@
+export const ReferenceCalibrationConfig = {
+  image: 'dopamine-reference.png',
+  projection: 'XY',
+  origin: {
+    x: 0,
+    y: 0,
+    z: 0
+  },
+  transform: {
+    scale: 1,
+    rotation: 0,
+    offsetX: 0,
+    offsetY: 0
+  },
+  calibrationPoints: []
+};
