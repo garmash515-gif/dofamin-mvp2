358e0088b46ad998ec2726f605347a7f1352f067
garmash515-gif <garmash515@gmail.com>
2026-08-12 07:22:26 +0300

    15.4.3 connect atom interaction

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createAppScene } from './scene/scene.js?v=13.8.2';
 import { createDopamineMolecule } from './molecule/dopamine.js?v=13.8.2';
 import { createFireflies } from './effects/fireflies.js?v=13.8.2';
 import { playMoleculeIntro } from './effects/transitions.js';
+import { createAtomInteraction } from './interaction/atomInteraction.js?v=15.4.3';
 
 const app = createAppScene(document.querySelector('#stage'));
 
@@ -12,5 +13,12 @@ const intro = playMoleculeIntro(molecule.molecule);
 app.transitions = [intro];
 
 app.cameraController.showMolecule(molecule.molecule, 1.6);
+
+createAtomInteraction({
+  camera: app.camera,
+  renderer: app.renderer,
+  molecule: molecule.molecule,
+  cameraController: app.cameraController
+});
 
 app.start();
