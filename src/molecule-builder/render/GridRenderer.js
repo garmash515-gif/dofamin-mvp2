45763e6f5543d0bd2a8b9616c9822eed4ccd1f57
garmash515-gif <garmash515@gmail.com>
2026-08-12 22:26:53 +0300

    Connect grid renderer to Three.js scene

--- src/molecule-builder/render/GridRenderer.js
+++ src/molecule-builder/render/GridRenderer.js
@@ -1,13 +1,19 @@
+import * as THREE from 'three';
+
 export function createGridRenderer(scene, {
   size = 20,
   divisions = 20
 } = {}) {
   if (!scene) return null;
 
+  const gridHelper = new THREE.GridHelper(size, divisions);
+  scene.add(gridHelper);
+
   const grid = {
     type: 'grid-renderer',
     size,
     divisions,
+    object: gridHelper,
     visible: true
   };
 
